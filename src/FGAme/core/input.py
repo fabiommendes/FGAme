24989e6bdbbd36a9d925e36bf20e375daa1d0ce8
Fábio Macêdo Mendes <fabiomacedomendes@gmail.com>
2015-06-06 16:27:07 -0300

    suporte a mouse-button-up/down e mouse-long-press

--- src/FGAme/core/input.py
+++ src/FGAme/core/input.py
@@ -12,11 +12,15 @@ class Input(EventDispatcher):
     key_up = signal('key-up', 'key')
     key_down = signal('key-down', 'key')
     mouse_motion = signal('mouse-motion', num_args=1)
-    mouse_click = signal('mouse-click', 'button', num_args=1)
+    mouse_button_up = signal('mouse-button-up', 'button', num_args=1)
+    mouse_button_down = signal('mouse-button-down', 'button', num_args=1)
+    mouse_long_press = signal('mouse-long-press', 'button', num_args=1)
 
     def __init__(self):
         super(Input, self).__init__()
         self._longpress_keys = set()
+        self._longpress_mouse_buttons = set()
+        self._mouse_pos = (0, 0)
 
     def init(self):
         pass
@@ -38,18 +42,39 @@ class Input(EventDispatcher):
         '''Executa todos os callbacks de longpress para as teclas
         pressionadas'''
 
+        callback = self.trigger_long_press
         for key in self._longpress_keys:
-            self.trigger_long_press(key)
+            callback(key)
 
     def process_mouse_motion(self, pos):
-        '''Executa os callbacks acionados pelo movimento do mouse'''
+        '''Executa callbacks associados ao movimento do mouse'''
 
         self.trigger_mouse_motion(pos)
+        self._mouse_pos = pos
 
-    def process_mouse_click(self, pos, button):
-        '''Executa os callbacks acionados pelo movimento do mouse'''
+    def process_mouse_button_down(self, button, pos):
+        '''Executa callbacks associados a pressionar um botão do mouse'''
 
-        self.trigger_mouse_click(button, pos)
+        self.trigger_mouse_button_down(button, pos)
+        self._longpress_mouse_buttons.add(button)
+
+    def process_mouse_button_up(self, button, pos):
+        '''Executa callbacks associados a levantar um botão previamente
+        pressionado do mouse'''
+
+        self.trigger_mouse_button_up(button, pos)
+        self._longpress_mouse_buttons.discard(button)
+
+    def process_mouse_longpress(self):
+        '''Executa callbacks de long-press para os botões do mouse.
+
+        A posição do ponteiro é guardada automaticamente no processamento dos
+        sinais de 'mouse-motion'.'''
+
+        pos = self._mouse_pos
+        callback = self.trigger_mouse_long_press
+        for button in self._longpress_mouse_buttons:
+            callback(button, pos)
 
     ###########################################################################
     #             Passo de resposta a eventos executado em cada loop
