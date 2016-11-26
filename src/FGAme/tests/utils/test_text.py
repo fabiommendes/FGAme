fe892ec31f73962feb2b7d1f726ca5f51dffdad9
Gustavo Cavalcante <gustavo.cavalcante.oliveira@live.com>
2016-11-26 16:30:07 -0200

    Tests for test_text.py

--- src/FGAme/tests/utils/test_text.py
+++ src/FGAme/tests/utils/test_text.py
@@ -1,4 +1,17 @@
 from FGAme.utils.text import snake_case
+from FGAme.utils.util import popattr
 
 def test_snake_case():
     assert snake_case("TestSnakeCase") == "test_snake_case"
+
+def test_popattr():
+
+	from FGAme.physics.bodies import Body
+
+	b1 = Body(pos=(0, 30), mass=1)
+
+	result = popattr(b1,'position')
+
+	assert result == None 
+	## example of deletable attr on FGAme?
+
